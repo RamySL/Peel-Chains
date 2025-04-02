b1d04316ced9a1356d49c3f5761cdc6de1d049dc
Ramy <ramyyahiasail@gmail.com>
2025-04-02 17:32:21 +0200

    imporved main, improved name generation for html

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ from mixers import graph_from_tx, IterBFS, IterDPS
 from render_pyvis import render
 
 def main():
-    #txid = input("TXID : ").strip()
-    txid = None
+    txid = input("TXID : ").strip()
     if not txid:
         print("Erreur : Aucun identifiant de transaction saisi.")
         txid = '87c6dffb5e103e24295a134d2449dccf15ac7a6147f19f2a39731cf121668108'
@@ -11,7 +10,8 @@ def main():
     try:
         depth = 4
         graph = graph_from_tx(txid, depth, IterDPS())
-        dst =   f"../html/graph{depth}.html"
+        # Le nom du html généré est raltif à la transaction et à la profondeur du graphe
+        dst =   f"../html/graph_{txid[:5]}_{depth}.html"
         render(graph, dst)
         print(f"Le graphe a été généré et affiché dans {dst}.")
     except Exception as e:
